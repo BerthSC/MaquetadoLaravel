4c9b5baae3dfc4358fba7f14e573f3027ed34442
Bertha Solis Cortes <l22240005@smartin.tecnm.mx>
2025-11-14 14:36:26 -0600

    agregar maquetado en laravel v1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/principal', function(){
+    return view('AuthViews/principal');
+});
+
+Route::get('/nuevoE', function(){
+    return view('RegisterViews/nuevoEjidatario');
+});
+
+Route::get('/nuevaP', function(){
+    return view('RegisterViews/nuevaParcela');
+});
+
+Route::get('/nuevoArt', function(){
+    return view('RegisterViews/nuevoArticulo');
+});
+
+Route::get('/nuevoGast', function(){
+    return view('RegisterViews/nuevoGasto');
 });
